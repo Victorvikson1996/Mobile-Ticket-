a85280ec1ac10c4db204a9c1ffa580cfe868cc55
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2022-02-03 00:02:16 +0530

    FlatList Componets    🚀 🚀 🚀

--- screens/Featured.js
+++ screens/Featured.js
@@ -16,11 +16,13 @@ import {
   Button,
   SafeAreaView,
   TextInput,
+  ImageBackground,
 } from 'react-native'
 import styled from 'styled-components/native'
 
 import { dummyData, FONTS, SIZES, COLORS, icons, images } from '../constants'
 import { McAvatar, McText, McIcon } from '../components'
+import { FlatList } from 'react-native-gesture-handler'
 
 const SectionHeader = styled.View`
   padding: 16px ${SIZES.padding};
@@ -42,7 +44,32 @@ const SearchView = styled.View`
   margin-left: 9px;
   margin-right: 15px;
 `
+
+const SectionTitle = styled.View`
+  margin: 20px ${SIZES.padding};
+`
 const Featured = ({ navigation }) => {
+  const _renderItem = ({ item, index }) => {
+    return (
+      <View
+        style={{
+          marginLeft: index === 0 ? 30 : 20,
+          marginRight: index === dummyData.Events.length - 1 ? 30 : 0,
+        }}
+      >
+        <ImageBackground
+          resizeMethod="cover"
+          borderRadius={SIZES.radius}
+          source={item.image}
+          style={{
+            width: SIZES.width / 2 + 70,
+            height: SIZES.width / 2 + 70,
+            justifyContent: 'space-between',
+          }}
+        ></ImageBackground>
+      </View>
+    )
+  }
   return (
     <SafeAreaView style={styles.container}>
       {/* headersection */}
@@ -72,6 +99,19 @@ const Featured = ({ navigation }) => {
           <McIcon source={icons.filter} />
         </SearchView>
       </SectionSearch>
+      {/* Feature */}
+      <SectionTitle>
+        <McText>FEATURES</McText>
+      </SectionTitle>
+      <View>
+        <FlatList
+          horizontal
+          contentContainerStyle={{}}
+          keyExtractor={(item) => 'event_' + item.id}
+          data={dummyData.Events}
+          renderItem={_renderItem}
+        ></FlatList>
+      </View>
     </SafeAreaView>
   )
 }
